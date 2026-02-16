efe9b49fd6d53e08c318794d3f504047938eb5e9
NoaTheSoap <noa.sjoset@gmail.com>
2026-02-16 14:42:00 +0100

    Cleaning up some repetetive code
    Cleaning up repetetive code, attemted better error hadling and removed unused code

--- sonar.py
+++ sonar.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import logging
+from exceptions import *
 from sonar_devices import SonarDevices
 
 class Sonar:
@@ -35,110 +36,92 @@ class Sonar:
 
     def get_base_port(self):
         # Use ggEncryptedAddress: C:\ProgramData\SteelSeries\SteelSeries Engine 3\coreProps.json
-        try:
-            with open(self.core_props_path, "r") as core_props_file:
-                core_props = json.loads(core_props_file.read())
-                self.base_port = core_props['ggEncryptedAddress']
-        except FileNotFoundError:
-            logging.error("coreProps.json not found at: %s", self.core_props_path)
+        if not os.path.exists(self.core_props_path):
+            raise CorePropsNotFoundError(self.core_props_path)
+        with open(self.core_props_path, "r") as core_props_file:
+            core_props = json.loads(core_props_file.read())
+            self.base_port = core_props['ggEncryptedAddress']
+
 
     def get_sonar_port(self):
         data = requests.get("https://"+self.base_port+ "/subApps", verify=False)
+        if data.status_code != 200:
+            raise SonarConnectionError(self.base_port)
         data_json = json.loads(data.text)["subApps"]["sonar"]
 
+
         if not data_json["isEnabled"]:
-            logging.warning("Sonar not enabled")
-            return False
+            raise SonarNotEnabled()
 
         if not data_json["isReady"]:
-            logging.warning("Sonar not ready")
-            return False
+            raise SonarNotReady()
 
         if not data_json["isRunning"]:
-            logging.warning("Sonar not running")
-            return False
+            raise SonarNotRunning()
 
         self.sonar_port = data_json["metadata"]["webServerAddress"]
         if not self.sonar_port:
-            logging.error("WebServer not found")
-            return False
+            raise SonarConnectionError(self.sonar_port)
 
-        return True
 
 
     # Volume: Value between 0 and 1.
     def set_volume(self, channel, volume:float):
         channel_key = self.CHANNEL_MAPPING.get(channel)
         if not channel_key:
-            logging.warning("Invalid channel: %s", channel)
-            return False
-
+            raise InvalidChannel(channel)
         if volume < 0 or volume > 1:
-            logging.warning("Volume must be between 0 and 1 %s", volume)
-            return False
+            raise InvalidVolume(volume)
 
-        url = f"{self.sonar_port}/volumeSettings/classic/{channel_key}/Volume/{str(volume)}"
-        requests.put(url)
-        return True
+        self._put(f"volumeSettings/classic/{channel_key}/Volume/{str(volume)}")
 
     def mute_channel(self, channel, mute:bool):
         if not channel in self.channel_names:
-            logging.warning("Invalid channel: %s", channel)
+            raise InvalidChannel(channel)
 
         if channel == "chat":
             channel = "chatRender"
 
-        url = f"{self.sonar_port}/volumeSettings/classic/{channel}/Mute/{str(mute).lower()}"
-        requests.put(url)
+        self._put(f"volumeSettings/classic/{channel}/Mute/{str(mute).lower()}")
+
 
     def set_output_device(self, channel ,device_id):
         channel_key = self.CHANNEL_MAPPING.get(channel)
         if not channel_key:
-            logging.warning("Invalid channel: %s", channel)
-            return False
+            raise InvalidChannel(channel)
 
         if channel_key == "chatRender":
             channel_key = "chat"
-        url = f"{self.sonar_port}/classicRedirections/{channel_key}/deviceId/{device_id}"
-        response = requests.put(url)
+        self._put(f"classicRedirections/{channel_key}/deviceId/{device_id}")
 
-        if response.status_code != 200:
-            logging.error("Could not set output device %s", device_id)
-            return False
-        return True
 
     def set_input_device(self, device_id:str):
-        url = f"{self.sonar_port}/classicRedirections/mic/deviceId/{device_id}"
-        response = requests.put(url)
+        self._put(f"classicRedirections/mic/deviceId/{device_id}")
 
-        if response.status_code != 200:
-            logging.error("Could not set input device %s", device_id)
-            return False
-        return True
-
-
-    def post_changes(self, setting, old_value, new_value):
-        payload = {
-            "event": "Setting Changed",
-            "properties":{
-                "source": "slider",
-                "setting": setting,
-                "value_old": old_value,
-                "value_new": new_value
-            }
-        }
-
-        post = requests.post(f"{self.sonar_port}/analytics/track", json=payload)
 
     def get_volume(self, channel:str):
         channel_key = self.CHANNEL_MAPPING.get(channel)
         if not channel_key:
-            logging.warning("Invalid channel: %s", channel)
-            return None
+            raise InvalidChannel(channel)
 
-        url = f"{self.sonar_port}/volumeSettings/classic"
-        response = requests.get(url)
-        if response.status_code != 200:
-            return None
-        return response.json()["devices"][channel_key]["classic"]["volume"]
+        return self._get("volumeSettings/classic")["devices"][channel_key]["classic"]["volume"]
 
+
+    def _get(self, endpoint):
+        try:
+            response = requests.get(f"{self.sonar_port}/{endpoint}", timeout=2)
+        except requests.exceptions.RequestException as e:
+            raise SonarConnectionError(self.sonar_port) from e
+
+        if not response.ok:
+            raise SonarNotAccessible(response.status_code)
+        return response.json()
+
+    def _put(self, endpoint):
+        try:
+            response = requests.put(f"{self.sonar_port}/{endpoint}", timeout=2)
+        except requests.exceptions.RequestException as e:
+            raise SonarConnectionError(self.sonar_port) from e
+
+        if not response.ok:
+            raise SonarNotAccessible(response.status_code)
